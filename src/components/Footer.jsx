3c4f8b7cd241d7a4bfac4ea1ed2b4e179641ae40
ansinha <ansinha@extremenetworks.com>
2026-07-17 16:31:32 +0530

    feat: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer({ ipAddress }) {
   return (
     <footer className="app-footer">
       <p>
-        Built with ❤️ in India 🇮🇳 for the world 🌍 • <span id="ip-display">IP: {ipAddress}</span>
+        Built with ❤️ by 🇮🇳 for the 🌍 • <span id="ip-display">IP: {ipAddress}</span>
       </p>
     </footer>
   );
